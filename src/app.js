15de3f064e53629be9e2a0a8e03efd5820895749
Ziyad Bhombal <zeebhombal@gmail.com>
2024-03-15 21:42:58 +0530

    feat: add orbit controls
    * cover types of threejs camera
    * cover types of builtin camera controls
    * cover how to setup basic custom camera controls

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 import "src/app.css";
 import * as T from "three";
+// eslint-disable-next-line no-unused-vars
 import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+console.log(OrbitControls);
+//
+// Cursor
+//
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+
+  // console.log(cursor.x, cursor.y);
+});
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -14,29 +33,70 @@ scene.add(group);
 
 const cube = new T.Mesh(
   // A uniform sized box i.e. a cube
-  new T.BoxGeometry(1, 1, 1),
+  new T.BoxGeometry(1, 1, 1, 5, 5, 5),
   // Material for the box
   new T.MeshBasicMaterial({ color: 0xff4646 })
 );
 
-cube.position.x = 1;
-
 group.add(cube);
 
 // AxesHelper
-const axesHelper = new T.AxesHelper(2);
-scene.add(axesHelper);
+// const axesHelper = new T.AxesHelper(2);
+// scene.add(axesHelper);
 
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// Camera
+// Perspective Camera
 
-const camera = new T.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(0, 0, 4);
+// fov
+// aspect ratio
+// near - any object closer than near wont show up
+// far - any object further than far wont show up
+const camera = new T.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+// Orthographic Camera
+
+// left
+// right
+// top
+// bottom
+
+// we multiply the left and right values
+// by the aspect ratio because our canvas is a rectangle
+// which causes the square cube to shrink in the x-axis to fit
+// the scene
+
+// modifying our camera this way enables us to have more room in the scene
+// on the x axis
+
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new T.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+
+camera.position.set(0, 0, 3);
+camera.lookAt(cube.position);
 scene.add(camera);
+
+// Controls
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 2;
+// controls.update();
 
 const rdr = new T.WebGLRenderer({
   canvas,
@@ -47,23 +107,21 @@ const rdr = new T.WebGLRenderer({
 rdr.setSize(sizes.width, sizes.height);
 rdr.render(scene, camera);
 
-// GSAP Stuff
-
-/*
-
-takes two params
-
-first - the object to modify
-second - the property on the object which is to be updated along with options like delay, duration etc.
-
-*/
-
-gsap.to(cube.position, { x: 2, duration: 1, delay: 1 });
-gsap.to(cube.position, { x: 0, duration: 1, delay: 2 });
-
 // Animation loop
 
 function tick() {
+  // const elapsedTime = clock.getElapsedTime();
+  // cube.rotation.y = elapsedTime;
+
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+
+  // camera.lookAt(cube.position);
+
+  // Update Controls
+  controls.update();
+
   rdr.render(scene, camera);
   // Provide callback to invoke on next frame
   window.requestAnimationFrame(tick);
